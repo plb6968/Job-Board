7b160a0509e7ecde86c9601ab0df5d2e9c28d05f
Phillip Black <plb6968@gmail.com>
2022-12-21 16:48:50 -0600

    polishing code

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -14,21 +14,13 @@ export default function CommentCard({user, comment, handleEdit, handleDelete, ha
     setEditedComment({...editedComment, [evt.target.name]: evt.target.value})
   }
 
-  function handleEdit() {
-    setEditPressed(true);
-  }
-
-  function handleCancelEdit() {
-    setEditPressed(false);
-  }
-
   return (
     <>
     {editPressed ? 
       <tr>
           <td><input type="text" name="text" value={editedComment.text} placeholder={comment.text} onChange={handleChange}></input></td>
               <>
-                <td><button type="button" onClick={() => {handleEditComment(editedComment); setEditPressed(false)}}>Update</button></td>
+                <td><button type="button" onClick={() => {setEditPressed(false)}}>Update</button></td>
                 <td><button type="button" onClick={() => setEditPressed(false)}>Cancel</button></td>
               </>
       </tr> 
